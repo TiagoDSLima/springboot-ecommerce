1c66b40f1f0aac9cc8933670360b739f7f4883f4
TiagoDSLima <tiagodslima51@gmail.com>
2024-12-21 13:53:01 -0300

    criando camada de serviço

--- src/main/java/dev/tiago/ecommerce/springboot_ecommerce/repositories/UserRepository.java
+++ src/main/java/dev/tiago/ecommerce/springboot_ecommerce/repositories/UserRepository.java
@@ -1,8 +1,10 @@
 package dev.tiago.ecommerce.springboot_ecommerce.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import dev.tiago.ecommerce.springboot_ecommerce.entites.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }

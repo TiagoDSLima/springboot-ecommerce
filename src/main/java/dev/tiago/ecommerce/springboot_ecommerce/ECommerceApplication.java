92fb710382b1b16db28f5ceb266c08766fd20b7d
TiagoDSLima <tiagodslima51@gmail.com>
2024-12-21 03:31:29 -0300

    Iniciando o projeto

---
+++ src/main/java/dev/tiago/ecommerce/springboot_ecommerce/ECommerceApplication.java
@@ -0,0 +1,13 @@
+package dev.tiago.ecommerce.springboot_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApplication.class, args);
+	}
+
+}

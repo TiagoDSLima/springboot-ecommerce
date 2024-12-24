83de28f7a321039be32a14dc45d48b0bd043742d
TiagoDSLima <tiagodslima51@gmail.com>
2024-12-24 16:09:21 -0300

    tratando execções das deleções de user

--- src/main/java/dev/tiago/ecommerce/springboot_ecommerce/services/UserService.java
+++ src/main/java/dev/tiago/ecommerce/springboot_ecommerce/services/UserService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import dev.tiago.ecommerce.springboot_ecommerce.entites.User;
 import dev.tiago.ecommerce.springboot_ecommerce.repositories.UserRepository;
+import dev.tiago.ecommerce.springboot_ecommerce.services.exceptions.DatabaseException;
 import dev.tiago.ecommerce.springboot_ecommerce.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +32,14 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+	    if (!repository.existsById(id)) {
+	        throw new ResourceNotFoundException(id);
+	    }
+	    try {
+	    	repository.deleteById(id);
+	    } catch (DataIntegrityViolationException e) {
+	    	throw new DatabaseException(e.getMessage());
+	    }
 	}
 	
 	public User update(Long id, User obj) {

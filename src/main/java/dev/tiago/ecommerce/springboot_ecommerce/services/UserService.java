4e54c77ef6bb34c5ddb37794b98d27fe107aabac
TiagoDSLima <tiagodslima51@gmail.com>
2024-12-24 16:22:24 -0300

    tratando excecções ao atualizar user

--- src/main/java/dev/tiago/ecommerce/springboot_ecommerce/services/UserService.java
+++ src/main/java/dev/tiago/ecommerce/springboot_ecommerce/services/UserService.java
@@ -11,6 +11,7 @@ import dev.tiago.ecommerce.springboot_ecommerce.entites.User;
 import dev.tiago.ecommerce.springboot_ecommerce.repositories.UserRepository;
 import dev.tiago.ecommerce.springboot_ecommerce.services.exceptions.DatabaseException;
 import dev.tiago.ecommerce.springboot_ecommerce.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -43,9 +44,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
